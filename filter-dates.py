257276f81f24f1edbffd28d2ee4e5c372b70b745
zameerlovesmath <75948807+zameerlovesmath@users.noreply.github.com>
2023-10-22 15:46:12 -0400

    Update filter-dates.py

--- filter-dates.py
+++ filter-dates.py
@@ -3,13 +3,13 @@ import pandas as pd
 input_file_path = "input-permit-data.csv"
 output_file_path = "filtered-data.csv"
 
-date_threshold = input("Enter the date threshold (will filter out everything before this) in MM/DD/YYYY format (eg 01/01/2023): ")
+date_threshold = input("Enter the date threshold (will filter out everything before this) in MM-DD-YYYY format (eg 01-01-2023): ")
 
 df = pd.read_csv(input_file_path)
 
-df["Issuance Date"] = pd.to_datetime(df['Issuance Date'], format='%m/%d/%Y')
+df["Filing Date"] = pd.to_datetime(df['Filing Date'], format='mixed')
 
-filtered_df = df[df["Issuance Date"] > pd.to_datetime(date_threshold, format='%m/%d/%Y')]
+filtered_df = df[df["Filing Date"] > pd.to_datetime(date_threshold, format='%m-%d-%Y')]
 
 filtered_df.to_csv(output_file_path, index=False)
 
