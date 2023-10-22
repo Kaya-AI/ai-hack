866adee1f165aad8cded90177ce26bbe36d2cc07
Joshua Cohen <65085812+joshuajohncohen@users.noreply.github.com>
2023-10-22 18:04:16 -0400

    Fix memory issue

--- ask_ai.py
+++ ask_ai.py
@@ -7,9 +7,15 @@ import os
 memory = ConversationBufferMemory(memory_key="chat_history")
 
 agent = create_csv_agent(
-    ChatOpenAI(model="gpt-4", temperature=0, openai_api_key=os.environ["OPENAI_API_KEY"]),
-    "permits-2020-onwards.csv",
-    verbose=True,
-    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-    memory=memory
+  ChatOpenAI(model="gpt-4", temperature=0, openai_api_key=os.environ["OPENAI_API_KEY"]),
+  "permits-2020-onwards.csv",
+  verbose=True,
+  agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+  memory=memory
 )
+
+def ask_agent(question):
+  memory.add_user_utterance(question)
+  response = agent.generate_response(question, memory.get_conversation_history())
+  memory.add_agent_utterance(response)
+  return response
