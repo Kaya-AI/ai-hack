73c355fc2ee1f003ad745bd3f9bf0d65288e2c4c
Joshua Cohen <65085812+joshuajohncohen@users.noreply.github.com>
2023-10-22 15:38:14 -0400

    Added prototype code for filtering by date

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ai-hack
+
+To install pandas and plotly for this project, open this directory in a terminal and run `pip install --requirement requirements.txt`
+
+To run the filtering code, put the data for the permits in this folder (locally) named `input-permit-data.csv` and run filter-dates.py
---
+++ filter-dates.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+input_file_path = "input-permit-data.csv"
+output_file_path = "filtered-data.csv"
+
+date_threshold = input("Enter the date threshold (will filter out everything before this) in MM/DD/YYYY format (eg 01/01/2023): ")
+
+df = pd.read_csv(input_file_path)
+
+df["Issuance Date"] = pd.to_datetime(df['Issuance Date'], format='%m/%d/%Y')
+
+filtered_df = df[df["Issuance Date"] > pd.to_datetime(date_threshold, format='%m/%d/%Y')]
+
+filtered_df.to_csv(output_file_path, index=False)
+
+print("Filtered data has been exported to filtered-data.csv")
