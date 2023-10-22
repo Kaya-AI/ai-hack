b037eb60abd0d740cbb8e39b40d781aaddaa06e7
Joshua Cohen <65085812+joshuajohncohen@users.noreply.github.com>
2023-10-22 15:51:11 -0400

    Add default of 2020 onwards

--- filter-dates.py
+++ filter-dates.py
@@ -3,13 +3,13 @@ import pandas as pd
 input_file_path = "input-permit-data.csv"
 output_file_path = "filtered-data.csv"
 
-date_threshold = input("Enter the date threshold (will filter out everything before this) in MM-DD-YYYY format (eg 01-01-2023): ")
+date_threshold = input("Enter the date threshold (will filter out everything before this) in MM-DD-YYYY format (eg 01-01-2023) or enter nothing to use 01-01-2020: ") or "01-01-2020"
 
 df = pd.read_csv(input_file_path)
 
 df["Filing Date"] = pd.to_datetime(df['Filing Date'], format='mixed')
 
-filtered_df = df[df["Filing Date"] > pd.to_datetime(date_threshold, format='%m-%d-%Y')]
+filtered_df = df[df["Filing Date"] >= pd.to_datetime(date_threshold, format='%m-%d-%Y')]
 
 filtered_df.to_csv(output_file_path, index=False)
 
