2696c9a89b98c1adaa95706b77da297a52f6e7df
zameerlovesmath <75948807+zameerlovesmath@users.noreply.github.com>
2023-10-22 17:56:42 -0400

    Added memory for AI

--- ask_ai.py
+++ ask_ai.py
@@ -4,9 +4,12 @@ from langchain.agents import create_csv_agent
 from langchain.memory import ConversationBufferMemory
 import os
 
+memory = ConversationBufferMemory(memory_key="chat_history")
+
 agent = create_csv_agent(
     ChatOpenAI(model="gpt-4", temperature=0, openai_api_key=os.environ["OPENAI_API_KEY"]),
     "permits-2020-onwards.csv",
     verbose=True,
     agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    memory=memory
 )
